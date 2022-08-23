b52d92815351ee4129548d46a2598da36e725bca
Eric <eric@smf.local>
2022-08-23 11:34:31 -0400

    complete chapter 3

---
+++ chap3/favorite_number.rb
@@ -0,0 +1,3 @@
+puts "What is your favorite number?"
+fav = gets.to_i
+puts "That's too bad. #{fav + 1} is a bigger and better favorite number."
---
+++ chap3/full_name_greeting.rb
@@ -0,0 +1,7 @@
+puts "What is your first name?"
+first = gets.chomp
+puts "What is your middle name?"
+middle = gets.chomp
+puts "What is your last name?"
+last = gets.chomp
+puts "I predict that your full name is #{first} #{middle} #{last}."
