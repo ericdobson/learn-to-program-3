5ebd3858a2943ed4fc52332e5cd1ecfca688af27
Eric <eric@smf.local>
2022-08-24 14:02:51 -0400

    forgot to commit a solit version before playing around

--- chap6/99_bottles.rb
+++ chap6/99_bottles.rb
@@ -1,20 +1,42 @@
-# not finished
+# I got greedy and didn't commit a solid working version before I tried to get fancy.
 
-100.times do |i|
-  if i != 99
-    bottles = 99 - i
+bot_qty = 3
+bot_lbl = bot_qty
+
+def set_plural_and_article
+  if @bot_qty == 1
+    @plr = ""
+    @art = "it"
   else
-    bottles = "no"
+    @plr = "s"
+    @art = "one"
+  end
+  puts "End set @plr = #{@plr}"
+end
+
+set_plural_and_article
+
+if bot_qty < 99; puts "[…]" end
+
+puts "Pre loop @plr = #{@plr}"
+
+while bot_qty >= 0 do
+  puts "#{bot_lbl}".capitalize + " bottle#{@plr} of beer on the wall, #{bot_lbl} bottle#{@plr} of beer."
+  if bot_qty == 0
+    puts "Go to the store and buy some more, 99 bottles of beer on the wall!"
+    break
+  else
+    print "Take #{@art} down, pass it around, "
+    bot_qty = bot_qty - 1
   end
 
-  if i != 98
-    bot_str = "bottles"
+  set_plural_and_article
+
+  if bot_qty == 0
+    bot_lbl = "no more"
   else
-    bot_str = "bottle"
+    bot_lbl = bot_qty
   end
 
-  print "#{bottles}".capitalize + " #{bot_str} of beer on the wall, "
-  puts "#{bottles} #{bot_str} of beer,"
-  print "take one down, pass it around, "
-  puts "#{bottles - 1} #{bot_str} of beer on the wall."
+  puts "#{bot_lbl} bottle#{@plr} of beer on the wall."
 end
