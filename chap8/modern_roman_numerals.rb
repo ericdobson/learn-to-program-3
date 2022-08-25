964a6d49148b5e7f02750909f68e7ba06e59e600
Eric <eric@smf.local>
2022-08-25 15:02:16 -0400

    modern numerals partial refactor

--- chap8/modern_roman_numerals.rb
+++ chap8/modern_roman_numerals.rb
@@ -1,56 +1,69 @@
+def write_nines(str_year, factor)
+  # puts "write_nines input: #{str_year}, #{factor}" # debug
+  values = {100 => "CM", 10 => "XC", 1 => "IX"}
+  str = str_year[0]
+  year = str_year[1]
+  if year/factor == 9
+    str += values[factor]
+    year -= (9 * factor)
+  end
+  # puts "write_nines output: #{str}, #{year}" # debug
+  [str, year]
+end
+
+def write_fives(str_year, factor)
+  values = {100 => "D", 10 => "L", 1 => "V"}
+  str = str_year[0]
+  year = str_year[1]
+  if year/(factor * 5) == 1
+    str += values[factor]
+    year = str_year[1] -= (5 * factor)
+  end
+  [str, year]
+end
+
+def write_fours(str_year, factor)
+  values = {100 => "CD", 10 => "XL", 1 => "IV"}
+  str = str_year[0]
+  year = str_year[1]
+  if year/factor == 4
+    str += values[factor]
+    year -= (4 * factor)
+  end
+  [str, year]
+end
+
+def write_singles(str_year, factor)
+  values = {100 => "C", 10 => "X", 1 => "I"}
+  str = str_year[0]
+  year = str_year[1]
+  if year/factor < 4
+    str += values[factor]*(year/factor)
+    year %= factor
+  end
+  [str, year]
+end
+
+def write_places(year, factor)
+  output = ["", year]
+  if factor == 1000
+    output[0] = "M"*(year/1000)
+  else
+    output = write_nines(output, factor)
+    output = write_fives(output, factor)
+    output = write_fours(output, factor)
+    output = write_singles(output, factor)
+  end
+  output[0].to_s
+end
+
 def new_roman(year)
   new_roman = ""
-  new_roman += "M"*(year/1000)
-  year %= 1000
-  if year/100 == 9
-    new_roman += "CM"
-    year -= 900
+  factors = [1000, 100, 10, 1]
+  factors.each do |factor|
+    new_roman += write_places(year, factor)
+    year %= factor
   end
-  if year/500 == 1
-    new_roman += "D"
-    year -= 500
-  end
-  if year/100 == 4
-    new_roman += "CD"
-    year -= 400
-  end
-  if year/100 < 4
-    new_roman += "C"*(year/100)
-    year %= 100
-  end
-  if year/10 == 9
-    new_roman += "XC"
-    year -= 90
-  end
-  if year/50 == 1
-    new_roman += "L"
-    year -= 50
-  end
-  if year/10 == 4
-    new_roman += "XL"
-    year -= 40
-  end
-  if year/10 < 4
-    new_roman += "X"*(year/10)
-    year %= 10
-  end
-  if year/1 == 9
-    new_roman += "IX"
-    year -= 9
-  end
-  if year/5 == 1
-    new_roman += "V"
-    year -= 5
-  end
-  if year/1 == 4
-    new_roman += "IV"
-    year -= 4
-  end
-  if year < 4
-    new_roman += "I"*year
-    year %= 1
-  end
-  # puts "Years remaining: #{year}"
   new_roman
 end
 
